2c92fcd89a0672278be65c42c46f72423efc5246
MazerOFFICIAL <mazeroxide@gmail.com>
2026-02-22 21:44:00 +0000

    Feb 22, 2026, 10:44 PM

--- script.js
+++ script.js
@@ -311,6 +311,7 @@ document.addEventListener('DOMContentLoaded', () => {
             DOM.audio.playPauseBtn.innerHTML = this.isPlaying 
                 ? '<svg viewBox="0 0 24 24"><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg>'
                 : '<svg viewBox="0 0 24 24"><path d="M8 5v14l11-7z"/></svg>';
+            DOM.audio.toggleBtn.classList.toggle('playing', this.isPlaying);
         },
         togglePlayerVisibility(show) {
             DOM.audio.container.classList.toggle('active', show);
